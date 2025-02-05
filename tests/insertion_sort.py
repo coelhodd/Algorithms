c99c18a34c6cccad3e0fcbc10ba8deb7a6ac1983
Davi Coelho <contato.coelhodd@gmail.com>
2025-02-04 23:18:22 -0300

    i have no idea lmao

--- tests/insertion_sort.py
+++ tests/insertion_sort.py
@@ -1,13 +1,11 @@
 def insertion_sort(a_list):
-    for i in range(1, len(a_list)): # 1 até o último índice, ou seja, pega todos os índices da lista
-        value = a_list[i]           # define uma variável chamada value para guardar o valor no índice atual i
-        j = i
-        while j > 0 and a_list[j - 1] > value:      # enquanto j(i) for maior que zero e o valor anterior for maior que o atual
-            a_list[j] = a_list[j - 1]               # então o valor atual será o valor anterior
-            j -= 1                                  # decrementa j(i)
-        a_list[j] = value                           
+    for i in range(1, len(a_list)):             # grabs the whole list(indexes)
+        value = a_list[i]
+        while i > 0 and a_list[i-1] > value:    # while not at the start of the list and the previous value is bigger than current
+            a_list[i] = a_list[i-1]             # searches for the right place for current value
+            i -= 1
+        a_list[i] = value                       # places it when out of loop(meaning the correct place was found)
     return a_list
 
 
-
-print(insertion_sort([9, 8, 7, 6]))
+print(insertion_sort([5, 4, 3, 2, 1]))
